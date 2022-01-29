3d56df70d15d2f731d709d1b465f71b96f48833c
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-01-29 12:15:42 -0300

    antes del paginado

--- pokeapi/PI-Pokemon-main/api/src/metodos/PedidoHome.js
+++ pokeapi/PI-Pokemon-main/api/src/metodos/PedidoHome.js
@@ -1,37 +1,48 @@
 const axios = require("axios");
 const { Router } = require("express");
-
+const { Pokemon, Tipo  } = require('../db')
 
 const PedidoHome = async () =>{
 
+  const DataBase= await Pokemon.findAll({
+    attributes:["name" , "id"] ,
+       include: {
+      model: Tipo,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+
     try{
 const request = await axios('https://pokeapi.co/api/v2/pokemon?offset=20&limit=40')
-    
    infoHome = request.data.results
      const Home = [];
-     
-     for(var i = 0 ; i<infoHome.length ; i++){
+     for (var i = 0 ; i<infoHome.length ; i++){
       const {url} = infoHome[i];
       const {data} = await axios(url)
       Home.push({
+      id: data.id,
     name : data.name,
-    spriteSrc: data.sprites. front_default,
-    types: data.types.map((e) => e.type.name),
+    spriteSrc: data.sprites.front_default,
+    Tipos: data.types.map((e) => e.type.name),
       })
      }
- return Home; 
-    }catch(e){
+      allPokes = [...Home,...DataBase]
   
- console.log(e)
+     return allPokes;
+    } catch (e){
+  
+    
 
     }
 
-
+  
 
 
 
 
 }
-
 
 module.exports = {PedidoHome}
