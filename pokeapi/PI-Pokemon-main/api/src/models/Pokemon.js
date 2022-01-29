b4a6c50be2a6b7c306b60e9ee0cf23f3595d6680
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-01-29 12:17:35 -0300

    antes del paginado

--- pokeapi/PI-Pokemon-main/api/src/models/Pokemon.js
+++ pokeapi/PI-Pokemon-main/api/src/models/Pokemon.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -33,14 +33,14 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
-    weight: {
+    weigth: {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
     height: {
       type: DataTypes.INTEGER,
       allowNull: true,
-    },
+    }
   
  },
  )};
