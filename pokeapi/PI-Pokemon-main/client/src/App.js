3fc945bf1d8525666eaa46a5346c52cd61aaee3c
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-01-29 12:21:51 -0300

    antes del paginado

--- pokeapi/PI-Pokemon-main/client/src/App.js
+++ pokeapi/PI-Pokemon-main/client/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import React from 'react';
+import  NavBar  from './componentes/NavBar'; 
+import Home from './componentes/Home';
+import { Route } from 'react-router-dom';
+import PokemonDetail  from './componentes/pokemonDetail';
+import SearchBar from './componentes/SearchBar';
+import CreatePokemon from './componentes/CreatePokemon';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
-  );
+    <React.Fragment>
+      <NavBar/>
+    
+  <Route exact path="/Home" component={Home}/>
+  <Route exact path="/pokemons/:id" component={PokemonDetail}/>
+  <Route exact path="pokemons/name?" component={SearchBar}/>
+  <Route exact path="/Home/createpokemon" component={CreatePokemon}/>
+
+  </React.Fragment>
+);
 }
 
 export default App;
