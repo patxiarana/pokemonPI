fa81f41cacb0aadb9c4076d1d207e9e9639b0c6c
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-01-29 12:23:58 -0300

    antes del paginado

--- pokeapi/PI-Pokemon-main/client/src/index.js
+++ pokeapi/PI-Pokemon-main/client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
+ <Provider store = {store}>
+ <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+</BrowserRouter>
+</Provider>,
+ document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
