34dd16640dc6f48191f5b0601caec922c4b52afd
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-01-30 04:13:52 -0300

    falta css

--- pokeapi/PI-Pokemon-main/client/src/App.js
+++ pokeapi/PI-Pokemon-main/client/src/App.js
@@ -2,24 +2,23 @@ import './App.css';
 import React from 'react';
 import  NavBar  from './componentes/NavBar'; 
 import Home from './componentes/Home';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import PokemonDetail  from './componentes/pokemonDetail';
 import SearchBar from './componentes/SearchBar';
 import CreatePokemon from './componentes/CreatePokemon';
-
+import logo from './componentes/img/mewLogo.jpg'
 
 
 function App() {
   return (
-    <React.Fragment>
+   <BrowserRouter>
       <NavBar/>
-    
+   
   <Route exact path="/Home" component={Home}/>
   <Route exact path="/pokemons/:id" component={PokemonDetail}/>
   <Route exact path="pokemons/name?" component={SearchBar}/>
   <Route exact path="/Home/createpokemon" component={CreatePokemon}/>
-
-  </React.Fragment>
+  </BrowserRouter>
 );
 }
 
