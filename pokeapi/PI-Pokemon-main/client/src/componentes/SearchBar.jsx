d234631ea07413e430a5f49c4c4701a2e5dce4a0
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-02-01 17:55:52 -0300

    completado

--- pokeapi/PI-Pokemon-main/client/src/componentes/SearchBar.jsx
+++ pokeapi/PI-Pokemon-main/client/src/componentes/SearchBar.jsx
@@ -2,32 +2,48 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { SearchName } from "../actions";
+import "../componentes/syles.css/SearchBar.css"
+
+export default function SearchBar() {
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+  
+    function handleInputChange(e) {
+      e.preventDefault();
+      setName(e.target.value);
+      //console.log(name);
+    }
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      dispatch(SearchName(name));
+    }
+  
+    return (
+      <div className="searchBar">
+        <input
+          className="barra"
+          type="text"
+          placeholder="buscar..."
+          onChange={(e) => handleInputChange(e)}
+        />
+        <br />
+        <button type="submit" onClick={(e) => handleSubmit(e)} className="btnSearch">
+          BUSCAR
+        </button>
+      </div>
+    );
+  }
 
 
- export default function SearchBar(){
-  const dispatch = useDispatch;
-  const [name, setName] = useState('')
-
-    const handelchange= (e) =>{
-   e.preventDefault()
-    setName(e.target.value)
-}
-const handelsubmit = (e) =>{
-    e.preventDefault()
-    dispatch(SearchName(name))
-}
-
-return(
-<div>
-
-<input
-type="text"
-placeholder="buscar..."
-onChange={handelchange}
-/>
-<button type="submit" onClick={handelsubmit}>buscar</button>
 
 
-</div>
-) 
-};
+
+
+
+
+
+
+
+
+
