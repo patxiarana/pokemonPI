945ffb3dd7db537cf0bf3f797dc91c4b2f77a58d
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-02-01 17:51:42 -0300

    completado

--- pokeapi/PI-Pokemon-main/client/src/App.js
+++ pokeapi/PI-Pokemon-main/client/src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import React from 'react';
-import  NavBar  from './componentes/NavBar'; 
 import Home from './componentes/Home';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import PokemonDetail  from './componentes/pokemonDetail';
 import SearchBar from './componentes/SearchBar';
 import CreatePokemon from './componentes/CreatePokemon';
-import logo from './componentes/img/mewLogo.jpg'
-
+import landingPage from './componentes/landignpage';
 
 function App() {
   return (
    <BrowserRouter>
-      <NavBar/>
-   
+  <landingPage/>
+   <Route exact path="/" component={landingPage}/>
   <Route exact path="/Home" component={Home}/>
   <Route exact path="/pokemons/:id" component={PokemonDetail}/>
   <Route exact path="pokemons/name?" component={SearchBar}/>
