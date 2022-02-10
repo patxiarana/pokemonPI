816977e8b6b544ed6b7cb6e850f44beb078e6b61
patxi arana <90577230+patxiarana@users.noreply.github.com>
2022-02-10 19:04:38 -0300

    renderizado corregido

--- pokeapi/PI-Pokemon-main/client/src/componentes/pokemonDetail.jsx
+++ pokeapi/PI-Pokemon-main/client/src/componentes/pokemonDetail.jsx
@@ -3,26 +3,32 @@ import CardDetail from "./CardDetail"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getPoke } from "../actions"
-import Logo from '../componentes/img/logo.jpg'
+import Logo from '../componentes/img/ghost.jpg'
 import "../componentes/syles.css/Detail.css"
+import { NavLink } from "react-router-dom"
+import { Vaciamiento } from "../actions"
 
 const PokemonDetail = (props) => {
-
   
-    let  params  = props.match.params.id
-    const dispatch = useDispatch()
-    useEffect(() => {
-    dispatch(getPoke(params));
-      });
-
-      const PokemonD = useSelector((state) => state. pokemonID )
-       
-          
+  let  params  = props.match.params.id
+  let dispatch = useDispatch()
+  let pokemonId = useSelector((state) => state.detail)
+  console.log(params)
+  useEffect(()=>{
+    dispatch(getPoke(params))
+    dispatch(Vaciamiento(params))  
+  },[])
+   
+   
+      console.log(pokemonId)
     return (
+      <div>
+        
       <div className="cuerpo">
         <div className="CardSpace">   
-                    {
-                    PokemonD?.map((e) =>(
+                    { pokemonId.length < 1 ?   <p className="cargando">cargando...</p> 
+                    :
+                    pokemonId.map((e) =>(
                     <CardDetail  
                     key ={e.id}
                     id={e.id}
@@ -35,20 +41,22 @@ const PokemonDetail = (props) => {
                     height={e. height}
                     Tipos={  e.Tipos ? e.Tipos.map(el => el.name + '' ) : e.types ? e.types + '' :  e.Tipos1 }
                     spriteSrc={e.spriteSrc ? e.spriteSrc :  Logo}
-                   />
+                   /> 
+                    ))
+                   
+                  }
+
                 
-                    ))}
         </div>
         </div>  
+
+        </div>
     );
 
 
-
-
+  
 
     }
 
 
     export default  PokemonDetail;
-
-
